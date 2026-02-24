952df03409154cf4c75531a56658ac41bcbb14ae
javiersikkoo <jmariscalserrano@gmail.com>
2026-02-24 13:14:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
-from scraper.fetch_listings import fetch_listings
-from scraper.fetch_property_detail import fetch_property_detail
-
+from scraper.scraper_playwright import scrape_all_properties
 
 if __name__ == "__main__":
-
-    listings = fetch_listings()
-
-    print(f"\nTotal inmuebles encontrados: {len(listings)}\n")
-
-    # Solo probamos con los 3 primeros para no saturar
-    for property_data in listings[:3]:
-
+    properties = scrape_all_properties()
+    print(f"Total inmuebles encontrados: {len(properties)}")
+    for prop in properties:
         print("======================================")
-        print("URL:", property_data["url"])
-
-        if property_data["url"]:
-            detail = fetch_property_detail(property_data["url"])
-            print("Título:", detail["title"])
-            print("Habitaciones:", detail["bedrooms"])
-            print("Baños:", detail["bathrooms"])
-            print("Metros:", detail["meters"])
-            print("Imágenes:", len(detail["images"]))
+        print(f"URL: {prop['url']}")
+        print(f"Precio: {prop['price']}")
+        print(f"Imagen: {prop['image']}")
+        print(f"Zona: {prop['zone']}")
