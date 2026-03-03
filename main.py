670bf772c7b6002292c46e0843fa29dacc8b85eb
javiersikkoo <jmariscalserrano@gmail.com>
2026-03-03 09:54:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 # main.py
 from flask import Flask, jsonify
 from scraper import scrape_properties
+from cache import is_cache_valid, load_cache, save_cache
 
 app = Flask(__name__)
 
 @app.route("/properties", methods=["GET"])
 def get_properties():
-    properties = scrape_properties()
-    return jsonify(properties)
+    if is_cache_valid():
+        print("⚡ Usando datos cacheados")
+        return jsonify(load_cache())
+
+    print("♻️ Cache no válida, ejecutando scraping...")
+    data = scrape_properties()
+    save_cache(data)
+    return jsonify(data)
 
 if __name__ == "__main__":
-    print("🚀 Iniciando servidor y scraping bajo demanda...")
+    print("🚀 Servidor iniciado")
     app.run(host="0.0.0.0", port=10000)
