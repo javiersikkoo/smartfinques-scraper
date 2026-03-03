ed822171e4e7b40faec41e317470fef769ef9b59
javiersikkoo <jmariscalserrano@gmail.com>
2026-03-03 13:17:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-# main.py
 from scraper import scrape_all_properties, export_to_json, export_to_csv
 
-if __name__ == "__main__":
-    listado_url = "https://www.inmuebles.smartfinques.com/listado-de-propiedades/"
+START_URL = "https://www.inmuebles.smartfinques.com/busqueda/propiedades"  # URL de listado principal
 
-    propiedades = scrape_all_properties(listado_url, delay=1)
-
+def main():
+    propiedades = scrape_all_properties(START_URL)
+    print(f"Se han scrapeado {len(propiedades)} propiedades.")
     export_to_json(propiedades)
     export_to_csv(propiedades)
-
-    print(f"Se han scrapeado {len(propiedades)} propiedades.")
     print("Archivos 'propiedades.json' y 'propiedades.csv' generados.")
+
+if __name__ == "__main__":
+    main()
